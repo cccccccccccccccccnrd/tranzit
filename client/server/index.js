aa49514fd796611b447961e13f904ae30a0cf49e
cnrd <hello@conradweise.com>
2019-08-08 13:21:40 +0300

    fix

--- client/server/index.js
+++ client/server/index.js
@@ -25,7 +25,7 @@ async function start() {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port, host)
+  app.listen(port)
   console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
 }
 start()
