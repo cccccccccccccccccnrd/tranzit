b9bf57a3fdbd5651b58b6e48c2f4f2672110930c
cccccccccccccccccnrd <hello@conradweise.com>
2025-02-20 20:52:27 +0100

    roma x3

--- client/pages/_lang/events/_id.vue
+++ client/pages/_lang/events/_id.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="single single-event">
     <div class="indent">
-      <section v-if="!event[`name_${ $i18n.locale }`]" class="loading invert">
-        <p>{{ $t('loading') }}</p>
+      <section v-if="!event[`name_${$i18n.locale}`]" class="loading invert">
+        <p>{{ $t("loading") }}</p>
       </section>
-      <h2>{{ event[`name_${ $i18n.locale }`] }}</h2>
+      <h2>{{ event[`name_${$i18n.locale}`] }}</h2>
       <small class="info">
-        <span class="type">{{ event[`type_${ $i18n.locale }`] }}</span> — {{ date }}
+        <span class="type">{{ event[`type_${$i18n.locale}`] }}</span>
+        <span v-if="!event[`type_${$i18n.locale}`].startsWith('Roma')"
+          >— {{ date }}</span
+        >
       </small>
       <vue-markdown
         class="description"
-        :source="event[`description_${ $i18n.locale }`]"
+        :source="event[`description_${$i18n.locale}`]"
       />
       <img
         v-for="visual in event.media"
@@ -22,39 +25,43 @@
 </template>
 
 <script>
-import VueMarkdown from 'vue-markdown'
-import Strapi from 'strapi-sdk-javascript/build/main'
+import VueMarkdown from "vue-markdown";
+import Strapi from "strapi-sdk-javascript/build/main";
 
-const url = process.env.API_URL || 'http://localhost:1337'
-const strapi = new Strapi(url)
+const url = process.env.API_URL || "http://localhost:1337";
+const strapi = new Strapi(url);
 
 export default {
   components: {
-    VueMarkdown
+    VueMarkdown,
   },
   computed: {
-    date () {
-      const date = new Date(this.event.start)
+    date() {
+      const date = new Date(this.event.start);
       if (date.getHours() === 0 && date.getMinutes() === 0) {
-        return `${ date.getDate() }.${ date.getMonth() + 1 }.${ date.getFullYear() }`
+        return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
       } else {
-        return `${ date.getDate() }.${ date.getMonth() + 1 }.${ date.getFullYear() } ${ ('0' + date.getHours()).slice(-2) }:${ ('0' + date.getMinutes()).slice(-2) }`
+        return `${date.getDate()}.${
+          date.getMonth() + 1
+        }.${date.getFullYear()} ${("0" + date.getHours()).slice(-2)}:${(
+          "0" + date.getMinutes()
+        ).slice(-2)}`;
       }
-    }
+    },
   },
   methods: {
     src(path) {
-      return `${ url }${ path }`
-    }
+      return `${url}${path}`;
+    },
   },
-  async asyncData ({ params }) {
-    const response = await strapi.request('post', '/graphql', {
+  async asyncData({ params }) {
+    const response = await strapi.request("post", "/graphql", {
       data: {
         query: `query {
-          event(id: "${ params.id }") {
-            name_${ params.lang }
-            description_${ params.lang }
-            type_${ params.lang }
+          event(id: "${params.id}") {
+            name_${params.lang}
+            description_${params.lang}
+            type_${params.lang}
             start
             end
             media {
@@ -62,17 +69,17 @@ export default {
               url
             }
           }
-        }`
-      }
-    })
+        }`,
+      },
+    });
 
-    await new Promise(resolve => setTimeout(resolve, 300))
+    await new Promise((resolve) => setTimeout(resolve, 300));
 
     return {
-      event: response.data.event
-    }
-  }
-}
+      event: response.data.event,
+    };
+  },
+};
 </script>
 
 <style scoped>
@@ -92,4 +99,3 @@ small {
   text-transform: initial;
 }
 </style>
-
